a85aeda4c79430d8abd08cb4618b07bc6ea650f8
Daniel Hamilton <d.hamilton@ufl.edu>
2019-04-01 17:16:25 -0400

    added 'enable' to registers

--- src/reg.vhd
+++ src/reg.vhd
@@ -2,17 +2,25 @@ library ieee;
 use ieee.numeric_std.all;
 use ieee.std_logic_1164.all;
 
+-- PROVIDED BY GREG STITT
+
+-- UPDATES:
+-- 1. 	included an enable signal to determine when
+-- 		an input should be saved or not.
+
 entity reg is
   generic (
     WIDTH : positive := 8);
   port (
     clk    : in  std_logic;
     rst    : in  std_logic;
+    en	   : in  std_logic;
     input  : in  std_logic_vector(WIDTH-1 downto 0);
     output : out std_logic_vector(WIDTH-1 downto 0));
 end reg;
 
 architecture BHV of reg is
+	signal store : std_logic_vector(WIDTH-1 downto 0);
 begin
   
   process(clk,rst)
@@ -20,12 +28,18 @@ begin
   	
   	-- default values on reset
     if (rst = '1') then
-      output <= (others => '0');     
+      store <= (others => '0');     
        
-    elsif (clk'event and clk='1') then
-      output <= input;
+    -- only change the stored value if the 
+    -- enable is set.
+  	elsif (clk'event and clk='1') then
+  		if ( en = '1' ) then
+  			store <= input;
+  		end if;
             
     end if;    
-  end process;
+end process;
+
+	output <= store; -- only updates on rst or on enable
 
 end BHV;
